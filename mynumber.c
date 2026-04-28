bb4e530f1ac2f03d02c707351c107de5a469b35c
04leeyoungjae <leeyoungjae@pusan.ac.kr>
2026-04-28 22:25:48 +0900

    Step 4

--- mynumber.c
+++ mynumber.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 
+int is_prime(int a)
+{
+	if(a<2) return 0;
+	for(int i=2;i*i<=a;i++)
+	{
+		if(a%i==0) return 0;
+	}
+	return 1;
+}
+
 int main()
 {
 	for(int i=1;i<=100;i+=2) printf("%d\n",i);
 	for(int i=2;i<=100;i+=2) printf("%d\n",i);
+	for(int i=2;i<=100;i++)
+	{
+		if(is_prime(i)) printf("%d\n",i);
+	}
 	return 0;
 }
